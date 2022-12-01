601e9c64719423f26f5dd4631c8658a79da15cb4
Anees Fathima S <83805776+ane942@users.noreply.github.com>
2022-12-01 12:01:27 +0530

    Delete ElectrcianHiringSystem directory

--- ElectrcianHiringSystem/fullstack-backend/src/main/java/com/fs/fullstackbackend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fs.fullstackbackend.repository;
-
-import com.fs.fullstackbackend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
